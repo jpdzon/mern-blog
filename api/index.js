10cb645b1b840a64c1b7ef878858e9a6dcdd11c1
John Paul V. Dizon <jdpizon@gbox.adnu.edu.ph>
2025-05-26 02:54:14 +0800

    Almost Done

--- api/index.js
+++ api/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose");
 const User = require('./models/User');
+const Post = require('./models/Post')
 const bcrypt = require('bcryptjs')
 const app = express();
 const jwt = require ('jsonwebtoken');
 const cookieParser = require('cookie-parser'); 
+const multer = require('multer'); 
+const uploadMiddlware = multer({ dest: 'uploads/' })
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'abcdef';
@@ -13,6 +17,7 @@ const secret = 'abcdef';
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://jpdizon:yx0AyoGkWd5YMFXc@cluster0.avmqph9.mongodb.net/');
 
@@ -40,7 +45,11 @@ app.post('/login', async (req, res) => {
             //logged in
             jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) =>{
                 if (err) throw err;
-                res.cookie('token', token).json('ok');
+                res.cookie('token', token).json({
+                    id:userDoc._id,
+                    username,
+
+                });
             });
         }
         else{
@@ -59,9 +68,47 @@ app.get('/profile', (req,res) => {
 app.post('/logout', (req,res) => { 
   res.cookie('token', '').json('ok');
 });
+
+app.post('/post',uploadMiddlware.single('file') , async(req,res) => { 
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+  
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async(err,info) => {
+    if (err) throw err;
+     const {title, summary, content} = req.body;
+
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath,
+        author: info.id,
+   })
+      res.json({postDoc});
+  });
+
+});
+
+app.get('/post', async (req,res) => { 
+  res.json(await Post.find().populate('author',['username'])
+    .sort({createdAt: -1})
+    .limit(20)
+);
+});
+
+app.get('/post/:id', async (req,res) => { 
+    const{id} = req.params;
+    const postDoc = await Post.findById(id).populate('author',['username']);
+    res.json(postDoc);
+})
+
 app.listen(4000);
 
-
+ 
 /*
 database username and password:
 blog0

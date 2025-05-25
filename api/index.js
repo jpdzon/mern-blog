521b3621629dbf6377a459191f3b43659f0946e5
John Paul V. Dizon <jdpizon@gbox.adnu.edu.ph>
2025-05-25 15:12:39 +0800

    Initial commit

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://jpdizon:vU4E54h0gUAwLhok3@cluster0.zvlga5w.mongodb.net/');
+mongoose.connect('mongodb+srv://jpdizon:yx0AyoGkWd5YMFXc@cluster0.avmqph9.mongodb.net/');
 
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
@@ -32,21 +32,38 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-    const {username, password} = req.body;
+  const { username, password } = req.body;
 
-        const userDoc = await User.findOne({username});
-        const passOK = bcrypt.compareSync (password, userDoc.password); //compare password entered from password in the database
-        if(passOK){
-            //logged in
-            jwt.sign({username, id:userDoc._id}, secret, {}, (err,token) =>{
-                if (err) throw err;
-                res.cookie('token', token).json('ok');
-            });
-        }
-        else{
-            res.status(400).json ('wrong credentials');
-        }
+  try {
+    const userDoc = await User.findOne({ username });
+
+    if (!userDoc) {
+      return res.status(400).json('wrong credentials');
+    }
+
+    const passOK = bcrypt.compareSync(password, userDoc.password); // compare passwords
+
+    if (passOK) {
+      // logged in
+      jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+        if (err) throw err;
+        res
+          .cookie('token', token, {
+            httpOnly: true,
+            sameSite: 'lax', // or 'none' if using HTTPS
+            secure: false,   // set to true if using HTTPS
+          })
+          .json('ok');
+      });
+    } else {
+      res.status(400).json('wrong credentials');
+    }
+  } catch (err) {
+    console.error('Login error:', err);
+    res.status(500).json('internal server error');
+  }
 });
+
 
 app.get('/profile', (req,res) => {
   const {token} = req.cookies;
@@ -66,5 +83,5 @@ app.listen(4000);
 database username and password:
 blog0
 U4E54h0gUAwLhok3
-mongodb+srv://blog0:U4E54h0gUAwLhok3@blog0.clupmno.mongodb.net/ // edit ip whitelist added my IP address
+w // edit ip whitelist added my IP address
 */
